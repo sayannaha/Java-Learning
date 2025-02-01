fc54e844710cac1ffbf7a72f37ea39dd9cb2302b
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-02-01 20:43:06 +0530

    Delete test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        FindCategory first = new FindCategory();
-        String getCat = first.getCategory("MOBILE");
-        System.out.println("\nCategory is : " + getCat + ", MRP : Rs." + first.getPrice(getCat) + ", Delivery Date : "
-                + first.deliveryDate(getCat, 3));
-    }
-}
